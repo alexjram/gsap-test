f9af01fdf868ca9e2151140699ef2366eb94d0ce
Alexis Ramírez <ar@xrf.ai>
2026-02-17 08:45:32 -0400

    added pinned sidebar thingy

---
+++ src/contants/images.tsx
@@ -0,0 +1,9 @@
+import image1 from "@/assets/image-1.jpg";
+import image2 from "@/assets/image-2.jpg";
+import image3 from "@/assets/image-3.jpg";
+import image4 from "@/assets/image-4.jpg";
+import image5 from "@/assets/image-5.jpg";
+import image6 from "@/assets/image-6.jpg";
+import image7 from "@/assets/image-7.jpg";
+
+export const images = [image1, image2, image3, image4, image5, image6, image7];
--- src/views/level3/PinnedSidebarView.tsx
+++ src/views/level3/PinnedSidebarView.tsx
@@ -1,10 +1,90 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { images } from "@/contants/images";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export function PinnedSidebarView() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const fixedRef = useRef<HTMLDivElement>(null);
+  useGSAP(
+    () => {
+      if (!containerRef.current || !fixedRef.current) return;
+
+      ScrollTrigger.create({
+        trigger: containerRef.current,
+        start: "top top",
+        end: "bottom bottom",
+        pin: fixedRef.current,
+        scrub: 1,
+      });
+    },
+    { scope: containerRef },
+  );
   return (
     <div className="flex flex-col gap-4 p-4">
       <h1 className="text-3xl font-bold">Level 3: The "Pinned" Sidebar</h1>
-      <p className="text-muted-foreground">Design a layout where a piece of content stays "stuck" (pinned) on the left side of the screen while the text on the right side continues to scroll past it.</p>
+      <p className="text-muted-foreground">
+        Design a layout where a piece of content stays "stuck" (pinned) on the
+        left side of the screen while the text on the right side continues to
+        scroll past it.
+      </p>
       <div className="mt-8 rounded-lg border-2 border-dashed border-muted p-12">
-        <p className="text-center text-muted-foreground">Your implementation goes here</p>
+        <div
+          className="flex items-start bg-linear-to-br from-lime-100 to-green-100"
+          ref={containerRef}
+        >
+          <div
+            className="grow-0 shrink-0 px-8 w-1/5 bg-gray-800 text-white py-8 rounded-2xl mt-8 ml-8"
+            ref={fixedRef}
+          >
+            <ul>
+              <li className="text-lg hover:bg-white/30 mb-4 p-2 rounded-sm">
+                item 1
+              </li>
+              <li className="text-lg hover:bg-white/30 mb-4 p-2 rounded-sm">
+                item 2
+              </li>
+              <li className="text-lg hover:bg-white/30 mb-4 p-2 rounded-sm">
+                item 3
+              </li>
+              <li className="text-lg hover:bg-white/30 mb-4 p-2 rounded-sm">
+                item 4
+              </li>
+            </ul>
+          </div>
+          <div className="grid grid-cols-3 gap-4 p-8">
+            {images.map((im, i) => (
+              <Card key={i}>
+                <CardContent className="h-full">
+                  <img src={im} className="object-cover h-full rounded-md" />
+                </CardContent>
+              </Card>
+            ))}
+            {images.map((im, i) => (
+              <Card key={i}>
+                <CardContent className="h-full">
+                  <img src={im} className="object-cover h-full rounded-md" />
+                </CardContent>
+              </Card>
+            ))}
+            {images.map((im, i) => (
+              <Card key={i}>
+                <CardContent className="h-full">
+                  <img src={im} className="object-cover h-full rounded-md" />
+                </CardContent>
+              </Card>
+            ))}
+            {images.map((im, i) => (
+              <Card key={i}>
+                <CardContent className="h-full">
+                  <img src={im} className="object-cover h-full rounded-md" />
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
